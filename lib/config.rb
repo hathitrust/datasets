5898fd6339e7b2a0a9ad04130493cb9616c8e9f9
Ryan Rotter <rrotter@umich.edu>
2015-12-14 01:54:04 -0500

    config.rb

--- lib/config.rb
+++ lib/config.rb
@@ -1,2 +1,15 @@
-# config.rb
+require 'yaml'
 # load configuration from yaml file, provide access to said config
+
+module HTConfig  
+  @@config_file = "#{File.dirname(__FILE__)}/../etc/conf.yml"
+  @@config = YAML.load_file(@@config_file)
+
+  def self.config
+    return @@config
+  end
+  
+  def self.foo
+    return "hello"
+  end
+end
