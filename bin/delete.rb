9b6f20f2329ddc330bc5a48fb58c794d9e4412cd
Ryan Rotter <rrotter@umich.edu>
2015-12-18 13:24:17 -0500

    deletes again

--- bin/delete.rb
+++ bin/delete.rb
@@ -1,33 +1,25 @@
 # delete.rb
 # remove volumes from dataset based on rights criteria
 
-require_relative './database.rb'
-require_relative './volume.rb'
+require_relative '../lib/database.rb'
+require_relative '../lib/volume.rb'
 
 HTDB.items_to_delete.each do |row|
-  worker_pool.post do
-    volume = Volume.new(row[:namespace],row[:id])
-    volume.delete(row[:pd_us],row[:pd_world],row[:open_access])
-  end  
+  volume = Volume.new(row[:namespace],row[:id])
+  volume.delete(row[:pd_us],row[:pd_world],row[:open_access])
 end
 
 HTDB.items_to_delink_from_all.each do |row|
-  worker_pool.post do
-    volume = Volume.new(row[:namespace],row[:id])
-    volume.delink(row[:pd_us],row[:pd_world],row[:open_access])
-  end  
+  volume = Volume.new(row[:namespace],row[:id])
+  volume.delink(row[:pd_us],row[:pd_world],row[:open_access])
 end
 
 HTDB.items_to_delink_from_open_access.each do |row|
-  worker_pool.post do
-    volume = Volume.new(row[:namespace],row[:id])
-    volume.delink_open_access(row[:pd_us],row[:pd_world],row[:open_access])
-  end  
+  volume = Volume.new(row[:namespace],row[:id])
+  volume.delink_open_access(row[:pd_us],row[:pd_world],row[:open_access])
 end
 
 HTDB.items_to_delink_from_world.each do |row|
-  worker_pool.post do
-    volume = Volume.new(row[:namespace],row[:id])
-    volume.delink_pd_world(row[:pd_us],row[:pd_world],row[:open_access])
-  end  
+  volume = Volume.new(row[:namespace],row[:id])
+  volume.delink_pd_world(row[:pd_us],row[:pd_world],row[:open_access])
 end
