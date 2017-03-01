eb734f9ef25d482a42502fec4ef46c7e4c09757a
Bryan Hockey <bhock@umich.edu>
2017-03-01 15:26:17 -0600

    Volume repo now has separate methods for volumes whose rights have changed and whose zips have changed

--- lib/volume_repo.rb
+++ lib/volume_repo.rb
@@ -2,9 +2,19 @@ require "volume"
 
 # Interface for retrieving volumes from the rights database.
 class VolumeRepo
-  # Retrieve all unique volumes that have change since
-  # the given time.  The list is unordered.
-  # @param [Time] time A local time
+  # Retrieve all unique volumes that have had a change in
+  # their rights within the given time period.
+  # The list is unordered.
+  # @param [Time] start_time
+  # @param [Time] end_time
   # @return Set<Volume>
-  def changed_since(time); end
+  def rights_changed_between(start_time, end_time); end
+
+  # Retrieve all unique volumes that have had a content
+  # change within the given time period.
+  # The list is unordered.
+  # @param [Time] start_time
+  # @param [Time] end_time
+  # @return Set<Volume>
+  def zip_changed_between(start_time, end_time); end
 end
