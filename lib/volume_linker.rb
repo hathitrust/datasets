1e97a1fc196954d5fa21d62c8cf4621469d7d490
Bryan Hockey <bhock@umich.edu>
2017-03-01 15:26:50 -0600

    Add VolumeLinker, which manages creation/deletion of links in subsets. VolumeWriter is now an interface followed by VolumeCreator and VolumeLinker

---
+++ lib/volume_linker.rb
@@ -0,0 +1,45 @@
+require "filesystem"
+require "path_resolver"
+require "volume"
+require "volume_writer"
+require "pathname"
+
+# Writes and deletes symlinks within the subsets
+# to the superset.
+class VolumeLinker
+
+  # @param [PathResolver] dest_path_resolver
+  # @param [Filesystem] fs
+  def initialize(dest_path_resolver:, fs:)
+    @dest_path_resolver = dest_path_resolver
+    @fs = fs
+  end
+
+  # Create a link for the volume within the subset
+  # defined by the dest_path_resolver.
+  # This operation is idempotent.
+  # @param [Volume] volume
+  # @param [Pathname] src_path
+  def save(volume, src_path)
+    dest_path = dest_path_resolver.path(volume)
+    fs.mkdir_p dest_path.parent
+    fs.ln_s src_path, dest_path
+  end
+
+  # Delete a link for the volume within the
+  # subset defined by the dest_path_resolver.
+  # Then delete any empty parent directories
+  # in the subset recursively.
+  # This operation is idempotent.
+  # @param [Volume] volume
+  def delete(volume)
+    dest_path = dest_path_resolver.path(volume)
+    fs.remove(dest_path)
+    fs.rm_empty_tree(dest_path.parent)
+  end
+
+  private
+
+  attr_reader :dest_path_resolver, :fs
+
+end
--- lib/volume_writer.rb
+++ lib/volume_writer.rb
@@ -2,58 +2,18 @@ require "filesystem"
 require "volume"
 require "zip_writer"
 
-# Writes and deletes volumes to the filesystem
-# This is responsible for making all of the
-# *decisions* around whether or not to write or
-# delete files.
+# Writes and deletes volumes within the superset.
 class VolumeWriter
-
-  # @param [IPathResolver] dest_path_resolver
-  # @param [ZipWriter] writer
-  # @param [Filesystem] fs
-  def initialize(dest_path_resolver:, writer:, fs:)
-    @dest_path_resolver = dest_path_resolver
-    @writer = writer
-    @fs = fs
-  end
-
 
   # Save a volume to the filesystem.  This is
   # an idempotent operation.
   # @param [Volume] volume
-  # @param [String] src_path Path to the volume's source directory.
-  def save(volume, src_path)
-    dest_path = dest_path_resolver.path(volume)
-    unless fs.exists?(zip_file(dest_path))
-      fs.mkdir_p(dest_path)
-      writer.write(src_path, zip_file(dest_path)) # unclear if we should be passing zips or dirs here
-    end
-    unless fs.exists?(mets_file(dest_path))
-      fs.cp(mets_file(src_path), mets_file(dest_path))
-    end
-  end
+  # @param [Pathname] src_path Path to the volume's source directory.
+  def save(volume, src_path); end
 
   # Delete a volume from the filesystem.  This is
   # an idempotent operation.
   # @param [Volume] volume
-  def delete(volume)
-    dest_path = dest_path_resolver.path(volume)
-    if fs.exists?(dest_path)
-      fs.remove_entry_secure(dest_path)
-      # then walk up the pairtree deleting any empty dirs
-    end
-  end
-
-  def mets_file(path)
-    path + 'metz'
-  end
-
-  def zip_file(path)
-    path + 'foo.zip'
-  end
-
-  private
-
-  attr_reader :dest_path_resolver, :writer, :fs
+  def delete(volume); end
 
 end
