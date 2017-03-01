b21092bfe6fd9a0e07a7ee3add461ce325566422
Bryan Hockey <bhock@umich.edu>
2017-03-01 13:51:23 -0600

    PairtreePathResolver and PathResolver complete

--- lib/pairtree_path_resolver.rb
+++ lib/pairtree_path_resolver.rb
@@ -1,15 +1,16 @@
 require "path_resolver"
 require "volume"
+require "pairtree"
+require "pathname"
 
 class PairtreePathResolver < PathResolver
 
   def initialize(parent_dir)
-    @parent_dir = parent_dir
+    @parent_dir = Pathname.new(parent_dir)
   end
 
   def path(volume)
-    # Construct a path using parent_dir, Pairtree,
-    # and the volume's ns+id.
+    parent_dir + "obj" + volume.namespace + "pairtree_root" + Pairtree::Path.id_to_path(volume.id)
   end
 
 
--- lib/path_resolver.rb
+++ lib/path_resolver.rb
@@ -1,10 +1,15 @@
 require "volume"
+require "pathname"
 
 # Interface for constructing filesystem paths from volumes.
 class PathResolver
 
   # @param [Volume] volume
-  # @return [String]
+  # @return [Pathname]
   def path(volume); end
+
+  # Return the parent_dir this resolver was initialized with.
+  # @return [Pathname]
+  def parent_dir(volume); end
 
 end
