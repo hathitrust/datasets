d20d344d3472d27697b5323e1cc8585986d5dd34
Bryan Hockey <bhock@umich.edu>
2017-03-01 12:54:00 -0600

    Note VolumeRepo#changed_since dedupes the result (now returns a Set)

--- lib/volume_repo.rb
+++ lib/volume_repo.rb
@@ -2,7 +2,9 @@ require "volume"
 
 # Interface for retrieving volumes from the rights database.
 class VolumeRepo
+  # Retrieve all unique volumes that have change since
+  # the given time.  The list is unordered.
   # @param [Time] time A local time
-  # @return Array<Volume>
+  # @return Set<Volume>
   def changed_since(time); end
 end
