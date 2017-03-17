728bb27b1a9163e4c1778592a3827e6ca83528e2
Bryan Hockey <bhock@umich.edu>
2017-03-17 17:05:20 -0500

    Volume now coerces namespace,id->string, access_profile,right->symbol

--- lib/datasets/volume.rb
+++ lib/datasets/volume.rb
@@ -4,10 +4,10 @@ class Volume
     :access_profile, :right
 
   def initialize(namespace:, id:, access_profile:, right:)
-    @namespace = namespace
-    @id = id
-    @access_profile = access_profile
-    @right = right
+    @namespace = namespace.to_s
+    @id = id.to_s
+    @access_profile = access_profile.to_sym
+    @right = right.to_sym
   end
 
 end
