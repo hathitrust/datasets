5826d16437c78bca1631e31a149c45f1bb9c75c7
Bryan Hockey <bhock@umich.edu>
2017-03-30 11:16:20 -0500

    Add Datasets.config and Datasets::Configuration

--- lib/datasets.rb
+++ lib/datasets.rb
@@ -1,5 +1,6 @@
 require "datasets/version"
 
+require "datasets/configuration"
 require "datasets/filter"
 require "datasets/filesystem"
 require "datasets/pairtree_path_resolver"
@@ -11,3 +12,13 @@ require "datasets/volume_linker"
 require "datasets/volume_writer"
 require "datasets/zip_writer"
 
+module Datasets
+  class << self
+    def config
+      @config ||= Configuration.new
+    end
+    def config=(obj)
+      @config = obj
+    end
+  end
+end
---
+++ lib/datasets/configuration.rb
@@ -0,0 +1,26 @@
+require "logger"
+require "ostruct"
+require "yaml"
+
+module Datasets
+
+  class Configuration < OpenStruct
+
+    def initialize(hash = {})
+      super hash.merge(logger: NullLogger.new)
+    end
+
+    def self.from_yaml(path)
+      new(YAML.load(path))
+    end
+
+    private
+
+    class NullLogger < Logger
+      def initialize(*args); end
+      def add(*args, &block); end
+    end
+
+  end
+
+end
