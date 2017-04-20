af0684e2be23c516e53203a0578a66b7bff43621
Bryan Hockey <bhock@umich.edu>
2017-04-20 12:42:45 -0500

    Add Datasets::SchedulerJob

---
+++ spec/jobs/scheduler_job_spec.rb
@@ -0,0 +1,17 @@
+require_relative "../spec_helper"
+require_relative "../job_helper"
+require "jobs/scheduler_job"
+require "pathname"
+
+module Datasets
+
+  RSpec.describe SchedulerJob do
+    let(:profile) { :pd }
+
+    it_behaves_like "a job" do
+      let(:job) { described_class.new(profile) }
+    end
+  end
+
+end
+
