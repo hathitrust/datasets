547c20e24457684648282a02821c0a49df1af1a7
Aaron Elkiss <aelkiss@umich.edu>
2021-07-02 15:46:44 -0400

    Don't clobber existing output files
    Makes cron job idempotent; don't need to worry about multiple runs on the same day.

--- bin/grep_logs.sh
+++ bin/grep_logs.sh
@@ -5,19 +5,27 @@ set -x
 DATE=$(date -d "yesterday 13:00" '+%Y%m%d')
 LOGDIR=/htprep/datasets/logs
 DELETELOG=$LOGDIR/deletelog_$DATE.txt
+COMBINED_LOG = $LOGDIR/dataset-$DATE.log.bz2
 FULL_SET_LOGS=/htprep/datasets/ht_text/obj/ingest_log
-egrep 'profile: (force_)?full,.*: updated$' $LOGDIR/*-$DATE.log | perl -pi -e 's/.*volume: (\S+) .*/$1/' > $FULL_SET_LOGS/full_set_updates_$DATE.txt
+FULL_SET_UPDATES=$FULL_SET_LOGS/full_set_updates_$DATE.txt
+FULL_SET_DELETES=$FULL_SET_LOGS/full_set_deletes_$DATE.txt
 
-egrep ': removed$' $LOGDIR/*-$DATE.log | perl -pi -e 's/.*profile: (\S+), volume: (\S+) .*/$1\t$2/' > $DELETELOG
+if [[ ! -e $FULL_SET_UPDATES ]]; then
+  egrep 'profile: (force_)?full,.*: updated$' $LOGDIR/*-$DATE.log | perl -pi -e 's/.*volume: (\S+) .*/$1/' > $FULL_SET_UPDATES
+fi
 
-egrep '^full	' $DELETELOG | cut -f 2 > $FULL_SET_LOGS/full_set_deletes_$DATE.txt
-# egrep '^pd	' $DELETELOG > $LOGDIR/pd_deletes_$DATE.txt && echo 'email to ht-dataset-pd@umich.edu'
-# egrep '^pd_open	' $DELETELOG > $LOGDIR/pd_open_access_deletes_$DATE.txt && echo 'email to ht-dataset-pd-oa@umich.edu'
-# egrep '^pd_world	' $DELETELOG > $LOGDIR/pd_world_deletes.$DATE.txt && echo 'email to ht-dataset-pd-world@umich.edu'
-# egrep '^pd_world_open	' $DELETELOG > $LOGDIR/pd_world_open_deletes_$DATE.txt && echo 'email to ht-dataset-pd-world-oa@umich.edu'
+if [[ ! -e $DELETELOG ]]; then
+  egrep ': removed$' $LOGDIR/*-$DATE.log | perl -pi -e 's/.*profile: (\S+), volume: (\S+) .*/$1\t$2/' > $DELETELOG
+fi
 
-# concat & compress logs
-cat $LOGDIR/*-$DATE.log | bzip2 -9 > $LOGDIR/dataset-$DATE.log.bz2 && rm $LOGDIR/*-$DATE.log
+if [[ ! -e $FULL_SET_DELETES ]]; then
+  egrep '^full	' $DELETELOG | cut -f 2 > $FULL_SET_DELETES
+fi
+
+if [[ ! -e $COMBINED_LOG ]]; then
+  # concat & compress logs
+  cat $LOGDIR/*-$DATE.log | bzip2 -9 > $COMBINED_LOG && rm $LOGDIR/*-$DATE.log
+fi
 
 # email deletes; move delete log to 'sent' if success
 /usr/src/app/bin/notify.rb $LOGDIR/deletelog* && mv $LOGDIR/deletelog* $LOGDIR/delete_notifications_sent/
