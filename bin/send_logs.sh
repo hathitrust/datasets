3a3eed7f8adf45835e15078f4f0530ba1a20a5d2
Aaron Elkiss <aelkiss@umich.edu>
2024-05-07 16:35:43 -0400

    DEV-1086: use new notification script; pass SMTP_HOST

--- bin/send_logs.sh
+++ bin/send_logs.sh
@@ -5,5 +5,5 @@ set -x
 LOGDIR=/htprep/datasets/logs
 if compgen -G "$LOGDIR/deletelog*" > /dev/null; then
   # email deletes; move delete log to 'sent' if success
-  /usr/src/app/bin/notify.rb $LOGDIR/deletelog* && mv $LOGDIR/deletelog* $LOGDIR/delete_notifications_sent/
+  /usr/src/app/bin/datasets.rb notify --smtp-host $SMTP_HOST $LOGDIR/deletelog* && mv $LOGDIR/deletelog* $LOGDIR/delete_notifications_sent/
 fi
